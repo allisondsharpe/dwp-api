ada0ecaf88c66630005d3ccf9dd576463346ca73
Allison <allisonsharpe@ymail.com>
2016-03-24 17:10:57 -0400

    modified files

---
+++ test/__api.js
@@ -0,0 +1,48 @@
+var request = require('supertest');
+
+describe('API', function(){
+  var server;
+
+  beforeEach(function(){
+    server = require('../src/server.js');
+  });
+
+  afterEach(function(){
+    server.close();
+  });
+
+  it('/should return specified object.', function testHealth(done) {
+    request(server)
+      .get('/api/')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, { hello: "world"} ,done);
+  });
+
+  it('/status should return specified healthy:true.', function testHealth(done) {
+    request(server)
+      .get('/api/status')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, { healthy: 'true'} ,done);
+  });
+
+  it('/user/id should return a user object with id', function testHealth(done) {
+    var fakeUserID = 374;
+    request(server)
+      .get('/api/user/' + fakeUserID)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, { user: {id: fakeUserID}} ,done);
+  });
+
+  //Assignment: API'S and Unit Tests
+  it('/should return specified object.', function testHealth(done) {
+    request(server)
+      .get('/api/v1')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, { hello: "world"} ,done);
+  });
+});
+
